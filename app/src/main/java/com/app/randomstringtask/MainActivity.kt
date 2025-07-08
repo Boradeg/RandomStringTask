a2dc83e3eea3aecffccf42e1e5b57559a6657c30
Gokul <gokul.b@voxvalley.com>
2025-07-08 23:02:15 +0530

    Dependancy added.

--- app/src/main/java/com/app/randomstringtask/MainActivity.kt
+++ app/src/main/java/com/app/randomstringtask/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             RandomStringTaskTheme {
                 Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Androidxx",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
